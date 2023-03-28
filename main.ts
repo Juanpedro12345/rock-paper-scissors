f6279cc09f61ddd26ae943aa74691dcf644ec5fc
Juanpedro12345 <129174133+Juanpedro12345@users.noreply.github.com>
2023-03-28 13:41:43 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 10)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
